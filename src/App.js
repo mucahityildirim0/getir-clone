b0b4a402358ce64ed2949636dabea2d47306a529
Mücahit Yıldırım <mucahityildirim@Mucahit-MacBook-Air.local>
2022-06-17 22:20:22 +0300

    add components files and Header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import Header from "./components/Header";
+import HeroSection from "./components/HeroSection";
+import Categories from "./components/Categories";
+import Campaigns from "./components/Campaigns";
+import Favourite from "./components/Favourite";
+import MobileApp from "./components/MobileApp";
+import Cards from "./components/Cards";
+import Footer from "./components/Footer";
+
 function App() {
-  return <div className="App">Getir-Clone</div>;
+  return (
+    <div className="App">
+      <Header />
+      <HeroSection />
+      <Categories />
+      <Campaigns />
+      <Favourite />
+      <MobileApp />
+      <Cards />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@ module.exports = {
   mode: "jit",
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: (theme) => ({
+        "brand-color": "#4c3398",
+        "primary-brand-color": "#5d3ebc",
+        "secondary-brand-color": "#7849f7",
+      }),
+      gradientColorStops: (theme) => ({
+        "brand-color": "#4c3398",
+        "primary-brand-color": "#5d3ebc",
+        "secondary-brand-color": "#7849f7",
+      }),
+    },
   },
   plugins: [],
 };
